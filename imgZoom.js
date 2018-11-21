b2ecafeaf51a375d87a818640bb7055dbbdec1a7
liangxuecong <497513735@qq.com>
2018-11-21 20:04:36 +0800

    1.支持了图片的垂直居中（之前用line-height的实现不现实） 2.优化了边界处理

--- imgZoom.js
+++ imgZoom.js
@@ -6,16 +6,20 @@
  * @param {Object} options 配置参数
  **/
   class ImgZoom {
+    
     constructor(elImg, opts) {
       const self = this;
       this.elImg = elImg;
+      this.pnodeW = elImg.parentNode.offsetWidth;
+      this.pnodeH = elImg.parentNode.offsetHeight;
 
       this.options = {
         MAX_SCALE: 2,
         MIN_SCALE: 1,
         SCALE_PER_WHEEL: 0.25,
         ...opts
-      }
+      };
+      this.options.MIN_SCALE = this.options.MIN_SCALE > 1 ? this.options.MIN_SCALE : 1;
 
       this.mousedownTranslate = {
         x: 0,
@@ -27,21 +31,43 @@
 
       this.elImg.imgZoom = this;
       this.init();
-
     }
+
     init() {
-      console.log('init...')
+      this.calcCenter();
       this.elImg.addEventListener('mousewheel', mousewheelCb);
       this.elImg.addEventListener('mousedown', mousedownCb);
     }
-  }
 
+    destroy() {
+      this.elImg.removeEventListener('mousewheel', mousewheelCb);
+      this.elImg.removeEventListener('mousedown', mousedownCb);
+    }
+
+    /**
+     * @desc 让图片在容器内垂直居中
+     */
+    calcCenter() {
+      var __this = this;
+      __this.elImg.style.transitionDuration = '0s';
+
+      var image = new Image();
+      image.onload = function() {
+        image.onload = null;
+        __this.initTranslateX = -__this.elImg.offsetWidth/2;
+        __this.initTranslateY = -__this.elImg.offsetHeight/2;
+        setTranslate(__this.elImg, __this.initTranslateX, __this.initTranslateY);
+        
+        setTimeout(()=>{__this.elImg.style.transitionDuration = '0.5s';}, 500);
+      }
+      image.src = __this.elImg.src;
+    }
+  }
 
   /**
   * @desc 鼠标点击事件
   */
   function mousedownCb(e) {
-
     var $dom = e.target;
     var imgZoomInst = $dom.imgZoom;             // 该图片所关联的ImgZoom实例
     var translateRes = getTranslate($dom);      // translate结果 {x: xxx, y: xxx}
@@ -78,16 +104,25 @@
     var translateY = imgZoomInst.mousedownTranslate.y + (e.pageY - e.target.imgZoom.mousedownPageY);
 
     //边界校验
-    if(imgZoomInst.scale >= 1) {
-      var canMovePixX = $dom.offsetWidth * 0.5 * (imgZoomInst.scale - 1);
-      var canMovePixY = $dom.offsetHeight * 0.5 * (imgZoomInst.scale - 1);
-      if (translateX > canMovePixX) translateX = canMovePixX;
-      if (translateX < -canMovePixX) translateX = -canMovePixX;
-      if (translateY > canMovePixY) translateY = canMovePixY;
-      if (translateY < -canMovePixY) translateY = -canMovePixY;
+    var canMovePixX = $dom.offsetWidth * 0.5 * (imgZoomInst.scale - 1)
+    var canMovePixY = $dom.offsetHeight * 0.5 * (imgZoomInst.scale - 1)
+
+    if(imgZoomInst.scale*$dom.offsetWidth > imgZoomInst.pnodeW) {
+      if (translateX - imgZoomInst.initTranslateX > canMovePixX) translateX = imgZoomInst.initTranslateX + canMovePixX;
+      if (translateX - imgZoomInst.initTranslateX < -canMovePixX) translateX = imgZoomInst.initTranslateX - canMovePixX;
     } else {
-      return;
+      translateX = imgZoomInst.initTranslateX;
     }
+
+
+    if(imgZoomInst.scale*$dom.offsetHeight > imgZoomInst.pnodeH) {  
+      if (translateY - imgZoomInst.initTranslateY > canMovePixY) translateY = imgZoomInst.initTranslateY + canMovePixY;
+      if (translateY - imgZoomInst.initTranslateY < -canMovePixY) translateY = imgZoomInst.initTranslateY - canMovePixY;
+    } else {
+      translateY = imgZoomInst.initTranslateY;
+    }
+    console.log(`translateX=${translateX}, canMovePixX=${canMovePixX}, imgZoomInst.initTranslateX=${imgZoomInst.initTranslateX}, offsetWidth=${$dom.offsetWidth}`);
+
 
     //赋translate值给图片
     setTranslate($dom, translateX, translateY);
@@ -103,7 +138,6 @@
     $dom.removeEventListener('mouseleave', mouseoutCb);
     //拖拽图片时图片的 transition时间不能太大
     $dom.style.transitionDuration = '0.5s';
-
   }
 
   /**
@@ -114,26 +148,6 @@
   }
 
   /**
-  * @desc 防止mousewheel一秒N次触发
-  */
-  function proxyMouseWheel() {
-    var timer = null;
-    var isLock = false;
-    return function (e) {
-      if (isLock && !timer) {
-        timer = setTimeout(function () {
-          isLock = false;
-          timer = null;
-          //mousewheelCb(e)
-        }, 300)
-      } else if (!isLock) {
-        isLock = true;
-        mousewheelCb(e)
-      }
-    }
-  }
-
-  /**
   * @desc 鼠标滚轮事件回调
   */
   function mousewheelCb(e) {
@@ -141,9 +155,7 @@
     var imgZoomInst = elImg.imgZoom;
 
     var curScale = elImg.style.transform.match(/scale\((.*?)\)/) && elImg.style.transform.match(/scale\((.*?)\)/)[1] || 1;
-
     curScale = parseFloat(curScale);
-    console.log(`curScale=${curScale}`);
 
     if (e.wheelDelta > 0) {
       curScale += imgZoomInst.options.SCALE_PER_WHEEL;
@@ -155,11 +167,9 @@
 
     var value = elImg.style.transform && elImg.style.transform.replace(/scale\((.*)\)/, 'scale(' + curScale + ')') || 'scale(' + curScale + ')';
     var translateRes = getTranslate(elImg);
-    
+
     var offsetX = curScale >=1 ? e.offsetX : elImg.offsetWidth/2;
     var offsetY = curScale >=1 ? e.offsetY : elImg.offsetHeight/2;
-
-    console.warn(`offsetX=${offsetX}`);
 
     var translateX = (e.wheelDelta > 0 ? 1 : -1) * (e.target.offsetWidth / 2 - offsetX) * imgZoomInst.options.SCALE_PER_WHEEL + translateRes.x;
     var translateY = (e.wheelDelta > 0 ? 1 : -1) * (e.target.offsetHeight / 2 - offsetY) * imgZoomInst.options.SCALE_PER_WHEEL + translateRes.y;
@@ -168,14 +178,12 @@
     if(curScale >= 1) {
       var canMovePixX = elImg.offsetWidth * 0.5 * (curScale - 1);
       var canMovePixY = elImg.offsetHeight * 0.5 * (curScale - 1);
-      if (translateX > canMovePixX) translateX = canMovePixX;
-      if (translateX < -canMovePixX) translateX = -canMovePixX;
-      if (translateY > canMovePixY) translateY = canMovePixY;
-      if (translateY < -canMovePixY) translateY = -canMovePixY;
+      if (translateX - imgZoomInst.initTranslateX > canMovePixX) translateX = imgZoomInst.initTranslateX + canMovePixX;
+      if (translateX - imgZoomInst.initTranslateX < -canMovePixX) translateX = imgZoomInst.initTranslateX - canMovePixX;
+      if (translateY - imgZoomInst.initTranslateY > canMovePixY) translateY = imgZoomInst.initTranslateY + canMovePixY;
+      if (translateY - imgZoomInst.initTranslateY < -canMovePixY) translateY = imgZoomInst.initTranslateY - canMovePixY;
     }
     
-
-    //console.warn(`canMovePixX=${canMovePixX}, translateX=${translateX}`);
 
     value = ` translate(${translateX}px, ${translateY}px) scale(${curScale})`;
 
